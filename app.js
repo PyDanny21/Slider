15dc6ec5feb479dcdfa452055bfaa184e6f41e09
Daniel Quansah <114119228+PyDanny21@users.noreply.github.com>
2023-08-30 22:36:49 -0700

    Add files via upload
    Added Files

---
+++ app.js
@@ -0,0 +1,38 @@
+const menuItems= document.querySelector('.menu-items');
+
+const nav=document.querySelector('.navigation');
+
+
+menuItems.addEventListener('click',()=>{
+    menuItems.classList.toggle('active');
+    nav.classList.toggle('active');
+});
+
+const btns= document.querySelectorAll('.nav-btn');
+const slides= document.querySelectorAll('.img-slide');
+const contents= document.querySelectorAll('.content');
+
+var sliderNav=function(manual) {
+    btns.forEach((btn)=>{
+        btn.classList.remove('active');
+    });
+    
+    slides.forEach((slide)=>{
+        slide.classList.remove('active');
+    });
+    
+    contents.forEach((content)=>{
+        content.classList.remove('active');
+    });
+    btns[manual].classList.add('active');
+    slides[manual].classList.add('active');
+    contents[manual].classList.add('active');
+
+};
+
+    btns.forEach((btn, i)=>{
+        btn.addEventListener('click', ()=>{
+            console.log('clicked');
+            sliderNav(i);
+        });
+    });
